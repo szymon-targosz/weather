e77690f327f391cd597a84e4a25794c4ad465f28
Szymon Targosz <szymon7.t@gmail.com>
2018-11-21 12:13:41 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = env => {
+    const isProd = env === 'production';
+    return {
+        entry: ['@babel/polyfill', './src/scripts/index.js'],
+        output: {
+            path: path.resolve(__dirname, 'public/dist'),
+            filename: 'bundle.js'
+        },
+        mode: isProd ? 'production' : 'development',
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }, {
+                test: /\s?css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                }, {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                        url: false
+                    }
+                }, {
+                    loader: 'postcss-loader'
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
+        ],
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            publicPath: '/dist/',
+            watchContentBase: true,
+            historyApiFallback: true
+        },
+        devtool: isProd ? 'source-map' : 'inline-source-map'
+    }
+}
